26fa187e8e6092282a428a526e3fc0fd09a60be5
kwlee <keith.won.lee@gmail.com>
2017-11-02 16:26:01 -0700

    started switchboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; //newly added
+import { HttpModule } from '@angular/http'; //newly added
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, //newly added
+    HttpModule //newly added
   ],
   providers: [],
   bootstrap: [AppComponent]
